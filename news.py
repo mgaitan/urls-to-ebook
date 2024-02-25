5237ef05a17e2aa0ba9fc3f3b75c36d0e1a7c204
Martín Gaitán <gaitan@gmail.com>
2024-02-25 11:57:19 -0300

    first version

---
+++ news.py
@@ -0,0 +1,35 @@
+import subprocess
+import markdownify
+import json
+import os 
+from pathlib import Path
+from glob import glob
+import tempfile
+from datetime import datetime
+
+
+def parse(url):
+    cmd = ["mercury-parser", url]
+    data = json.loads(subprocess.check_output(cmd, text=True))
+    md = f"<h1>{data['title']}</h1>\n<p>{data['author'] or ''} - {data['url']}</p><hr>{data['content']}"
+    return markdownify.markdownify(md)
+
+
+def main():
+    urls = [url.strip() for url in os.getenv("URLS").split("\\n")]
+    with tempfile.TemporaryDirectory() as d:
+        for i, url in enumerate(urls):
+            if not url:
+                continue
+            print(f"processing {url}")
+            md = parse(url)
+            (Path(d) / f"{i:02}.md").write_text(md)
+
+        date = datetime.now().strftime('%Y-%m-%d')
+        output = f"news_{date}.epub"
+        title = f"News {date}"
+        subprocess.run(["pandoc", *glob(f"{d}/*.md"), "--metadata", f'title="{title}"', '--toc', '--toc-depth=1', '-o', output])   
+
+
+if __name__ == '__main__':
+    main()
